18999c184d59298a522b2331ceeb7aa2ae7cd909
DESKTOP-FKQTOHT\Romero <alerom02@ucm.es>
2021-03-10 17:54:13 +0100

    Prueba 2

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/V1.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/V3.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
